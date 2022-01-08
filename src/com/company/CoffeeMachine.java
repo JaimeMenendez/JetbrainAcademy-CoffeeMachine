0bdc5e417cd0796bf64e00017e32d4cf52d7c38d
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2022-01-07 19:00:41 -0600

    Removed repeated menu messages and warning fixed.

--- src/com/company/CoffeeMachine.java
+++ src/com/company/CoffeeMachine.java
@@ -9,7 +9,7 @@ public class CoffeeMachine {
     public boolean machineOn = true;
     private MachineStatus currentStatus = MachineStatus.MAIN_MENU;
     private int counterForFilling = 0;
-    private String currentMessage = "Write action (buy, fill, take, remaining, exit): ";
+    private String currentMessage;
 
     enum MachineStatus {
         MAIN_MENU, FILLING_RESOURCES, SELECTING_COFFEE
@@ -35,25 +35,21 @@ public class CoffeeMachine {
     public CoffeeMachine() {
         showInstructions();
     }
-
     public void action(String action) {
         if (machineOn) {
             switch (currentStatus) {
-                case MAIN_MENU:
-                    currentMessage = "Write action (buy, fill, take, remaining, exit): ";
-                    mainMenuActions(action);
-                    break;
-                case FILLING_RESOURCES:
+                case MAIN_MENU -> mainMenuActions(action);
+                case FILLING_RESOURCES -> {
                     fillMachine(action);
                     showInstructions();
-                    break;
-                case SELECTING_COFFEE:
+                }
+                case SELECTING_COFFEE -> {
                     Coffee coffee = selectCoffee(action);
                     if (coffee != null) {
                         prepareCoffee(coffee);
                     }
                     showInstructions();
-                    break;
+                }
             }
         } else {
             System.out.println("Coffee Machine is power off.");
@@ -61,34 +57,37 @@ public class CoffeeMachine {
     }
 
     private void showInstructions() {
+        switch (currentStatus) {
+            case SELECTING_COFFEE -> currentMessage = "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:";
+            case MAIN_MENU -> currentMessage = "Write action (buy, fill, take, remaining, exit): ";
+        }
         System.out.println(currentMessage);
     }
 
     private void mainMenuActions(String action) {
         System.out.println();
         switch (action) {
-            case "buy":
+            case "buy" -> {
                 currentStatus = MachineStatus.SELECTING_COFFEE;
-                currentMessage = "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:";
                 showInstructions();
-                break;
-            case ("fill"):
+            }
+            case ("fill") -> {
                 currentStatus = MachineStatus.FILLING_RESOURCES;
                 fillMachine(null);
                 showInstructions();
-                break;
-            case "take":
+            }
+            case "take" -> {
                 takeMoney();
                 showInstructions();
-                break;
-            case "remaining":
+            }
+            case "remaining" -> {
                 machineStatus();
                 showInstructions();
-                break;
-            case "exit":
+            }
+            case "exit" -> {
                 machineOn = false;
                 currentStatus = MachineStatus.MAIN_MENU;
-                break;
+            }
         }
     }
 
@@ -103,26 +102,17 @@ public class CoffeeMachine {
 
     private Coffee selectCoffee(String option) {
         currentStatus = MachineStatus.MAIN_MENU;
-        Coffee coffee;
-        switch (option) {
-            case "1":
-                coffee = new Coffee(4, 250, 16);
-                break;
-            case "2":
-                coffee = new Coffee(7, 350, 20, 75);
-                break;
-            case "3":
-                coffee = new Coffee(6, 200, 12, 100);
-                break;
-            default:
-                coffee = null;
-        }
-        return coffee;
+        return switch (option) {
+            case "1" -> new Coffee(4, 250, 16);
+            case "2" -> new Coffee(7, 350, 20, 75);
+            case "3" -> new Coffee(6, 200, 12, 100);
+            default -> null;
+        };
     }
 
     private void prepareCoffee(Coffee coffee) {
         boolean noError = true;
-        if (water - coffee.water < 0 && noError) {
+        if (water - coffee.water < 0) {
             System.out.println("Sorry, not enough water \n");
             noError = false;
         }
@@ -146,7 +136,6 @@ public class CoffeeMachine {
             System.out.println("I have enough resources, making you a coffee! \n");
             money += coffee.cost;
         }
-        currentMessage = "Write action (buy, fill, take, remaining, exit): ";
     }
 
     private void fillMachine(String input) {
@@ -154,27 +143,26 @@ public class CoffeeMachine {
             counterForFilling = 1;
             currentMessage = "Write how many ml of water you want to add: ";
         } else {
-            if (counterForFilling == 4 && input.matches("\\d+")) {
+            if (counterForFilling == 4 && (input != null && input.matches("\\d+"))) {
                 counterForFilling = 0;
                 disposableCups += Integer.parseInt(input);
                 System.out.println();
                 currentStatus = MachineStatus.MAIN_MENU;
-                currentMessage = "Write action (buy, fill, take, remaining, exit): ";
             }
 
-            if (counterForFilling == 3 && input.matches("\\d+")) {
+            if (counterForFilling == 3 && (input != null && input.matches("\\d+"))) {
                 counterForFilling = 4;
                 coffee += Integer.parseInt(input);
                 currentMessage = "Write how many disposable cups of coffee you want to add: ";
             }
 
-            if (counterForFilling == 2 && input.matches("\\d+")) {
+            if (counterForFilling == 2 && (input != null && input.matches("\\d+"))) {
                 counterForFilling = 3;
                 milk += Integer.parseInt(input);
                 currentMessage = "Write how many grams of coffee beans you want to add: ";
             }
 
-            if (counterForFilling == 1 && input.matches("\\d+")) {
+            if (counterForFilling == 1 && (input != null && input.matches("\\d+"))) {
                 counterForFilling = 2;
                 water += Integer.parseInt(input);
                 currentMessage = "Write how many ml of milk you want to add: ";
